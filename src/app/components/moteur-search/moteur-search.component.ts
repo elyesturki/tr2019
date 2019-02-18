bc676afb2696199c02790dce9dabf5bd0ebadbd1
Elyes Turki <elyestu@gmail.com>
2019-02-18 16:58:23 +0100

    Add autocomplete for origin and destination list

--- src/app/components/moteur-search/moteur-search.component.ts
+++ src/app/components/moteur-search/moteur-search.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 //import service ApiDataService
 import { ApiDataService } from '../../services/api-data.service';
 
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-moteur-search',
   templateUrl: './moteur-search.component.html',
-  styleUrls: ['./moteur-search.component.scss']
+  styleUrls: ['./moteur-search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MoteurSearchComponent implements OnInit {
+
+   public cities: any= [];
+   public model: any;
 
   constructor(private apiDataService: ApiDataService) { }
 
@@ -17,9 +25,31 @@ export class MoteurSearchComponent implements OnInit {
   }
 
   public getCities() {
-   this.apiDataService.getCities().subscribe((data) => {
-      console.log("data: ", data);
+   this.apiDataService.getCities().subscribe((data: any) => {
+     console.log("data: ",data)
+      for (let i=0; i<data.length ; i++) {
+        this.cities.push(data[i].name);
+      }
+      console.log("this.cities: ", this.cities);
     });
   }
+
+  public selectCityOrigin = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : this.cities.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+  public selectCityDestination = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : this.cities.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+
 
 }
